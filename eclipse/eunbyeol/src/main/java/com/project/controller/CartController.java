f0a8c2e48268c479b50be08cfa5c6db7dfabc529
cho eun byeol <whdmsquf22222@naver.com>
2021-03-31 22:05:48 +0900

    계속 빈 생성 오류

--- eclipse/eunbyeol/src/main/java/com/project/controller/CartController.java
+++ eclipse/eunbyeol/src/main/java/com/project/controller/CartController.java
@@ -24,31 +24,17 @@ import com.sun.javafx.collections.MappingChange.Map;
 @RequestMapping("/cart/*")
 public class CartController {
 	
-	@Resource(name="cartService")
-	private CartService cartService;
+	@Inject
+	CartService cartService;
 	
-	@RequestMapping("insert.do") //장바구니 추가
-
-	public String insertcart(@ModelAttribute CartVO vo, HttpSession session) {
-        String user_id=(String)session.getAttribute("user_id");
-	        vo.setUserid(user_id);
-	        int count = cartService.countCart(vo.getPid(), user_id);
-	        if(count == 0) {
-	        	cartService.insert(vo);
-	        } else {
-	        	cartService.updateCart(vo);
-	        }
-	        return "redirect:/cart/list.do"; 
-	    }
  
 	@RequestMapping("list.do") //장바구니 목록
  	public ModelAndView listcart(HttpSession session, ModelAndView mav) {
 	HashMap<String, Object> map = new HashMap<String,Object>();
     String user_id=(String)session.getAttribute("user_id");   
     List<CartVO> listcart=cartService.listCart(user_id);//장바구니 목록
-    int sumMoney=cartService.sumMoney(user_id);//금액 합계
-    map.put("sumMoney", sumMoney);    
-         map.put("sum", sumMoney); 
+    
+  
          map.put("list", listcart); 
          map.put("count", listcart.size()); 
          mav.setViewName("cart/list");
@@ -57,28 +43,4 @@ public class CartController {
          return mav;    
          
      }
-    
-	@RequestMapping ("update.do")// 장바구니 수정
-	public String update(@RequestParam int[] amount, @RequestParam int[] pid, HttpSession session) {
-	 String user_id=(String) session.getAttribute("user_id");
-	 for(int i=0; i<pid.length; i++) {
-		 CartVO vo = new CartVO();
-		 vo.setUserid(user_id);
-		 vo.setAmount(amount[i]);
-		 vo.setPid(pid[i]);
-		 cartService.modifyCart(vo);
-	 	}
-	 return "redirect:/cart/list.do";
-	}
-	@RequestMapping("delete.do") //장바구니 삭제
-	public String delete(@RequestParam int cart_id) {
-	 cartService.delete(cart_id);
-	 return "redirect:/cart/list.do";
-		}
- 
- 	}
- 
- 
-
-
-
+}
--- eclipse/eunbyeol/src/main/java/com/project/service/dao/CartDAO.java
+++ eclipse/eunbyeol/src/main/java/com/project/service/dao/CartDAO.java
@@ -6,26 +6,8 @@ import com.project.vo.CartVO;
 
 public interface CartDAO {
 	
-	List<CartVO> cartMoney();
-	
-	void insert(CartVO vo);
+
 	
 	List<CartVO> listCart(String user_id);
 	
-	void delete(int cart_id);
-
-	void update (int cart_id);
-	
-	int sumMoney(String user_id);
-	
-	int countCart(String user_id, int pid);
-	
-	void updateCart(CartVO vo);
-	
-	void modifyCart(CartVO vo);
-	
-	void insertcart(CartVO vo);
-	
-	
-
 }
